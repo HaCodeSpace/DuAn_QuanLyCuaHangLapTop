e1fd1a0dbab2d184e95e0a562edd9f05e4b99b14
Phong Trần Trung <phongtt2710@gmail.com>
2022-08-04 13:08:32 +0700

    sửa db hóa đơn

--- DAL/Model/KhuyenMai.cs
+++ DAL/Model/KhuyenMai.cs
@@ -11,7 +11,7 @@ namespace DAL.Model
         public string MaKM { get; set; }
         public string TenKhuyenMai { get; set; }
       //  public DateTime NgayTao { get; set; }
-        public int ChietKhau { get; set; }
+        public double ChietKhau { get; set; }
         public DateTime NgayBatDau { get; set; }
         public DateTime NgayKetThuc { get; set; }
         public Boolean TrangThai { get; set; }
