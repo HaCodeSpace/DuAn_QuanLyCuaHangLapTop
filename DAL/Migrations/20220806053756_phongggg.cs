c01dca36cc8f184311110a79a4c604debe5d7068
Phong Trần Trung <phongtt2710@gmail.com>
2022-08-06 12:49:48 +0700

    .

---
+++ DAL/Migrations/20220806053756_phongggg.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class phongggg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_nhanViens_nhanViens_MaQuanLi",
+                table: "nhanViens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_nhanViens_MaQuanLi",
+                table: "nhanViens");
+
+            migrationBuilder.DropColumn(
+                name: "MaQuanLi",
+                table: "nhanViens");
+
+            migrationBuilder.AddColumn<string>(
+                name: "QuanLiMaNV",
+                table: "nhanViens",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Vaitro",
+                table: "nhanViens",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_nhanViens_QuanLiMaNV",
+                table: "nhanViens",
+                column: "QuanLiMaNV");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_nhanViens_nhanViens_QuanLiMaNV",
+                table: "nhanViens",
+                column: "QuanLiMaNV",
+                principalTable: "nhanViens",
+                principalColumn: "MaNV",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_nhanViens_nhanViens_QuanLiMaNV",
+                table: "nhanViens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_nhanViens_QuanLiMaNV",
+                table: "nhanViens");
+
+            migrationBuilder.DropColumn(
+                name: "QuanLiMaNV",
+                table: "nhanViens");
+
+            migrationBuilder.DropColumn(
+                name: "Vaitro",
+                table: "nhanViens");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MaQuanLi",
+                table: "nhanViens",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_nhanViens_MaQuanLi",
+                table: "nhanViens",
+                column: "MaQuanLi");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_nhanViens_nhanViens_MaQuanLi",
+                table: "nhanViens",
+                column: "MaQuanLi",
+                principalTable: "nhanViens",
+                principalColumn: "MaNV");
+        }
+    }
+}
