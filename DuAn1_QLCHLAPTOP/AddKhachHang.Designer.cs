400501d4fbd5fc08bcb20c451687365aef339676
hà nguyễn <hanvph19236@fpt.edu.vn>
2022-08-09 14:15:04 +0700

    Update nhiều thứ

--- DuAn1_QLCHLAPTOP/AddKhachHang.Designer.cs
+++ DuAn1_QLCHLAPTOP/AddKhachHang.Designer.cs
@@ -37,16 +37,18 @@
             // 
             // tb_timkiem
             // 
-            this.tb_timkiem.Location = new System.Drawing.Point(122, 42);
+            this.tb_timkiem.Location = new System.Drawing.Point(139, 56);
+            this.tb_timkiem.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tb_timkiem.Name = "tb_timkiem";
-            this.tb_timkiem.Size = new System.Drawing.Size(288, 23);
+            this.tb_timkiem.Size = new System.Drawing.Size(329, 27);
             this.tb_timkiem.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(429, 42);
+            this.button1.Location = new System.Drawing.Point(490, 56);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "Tìm kiếm";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,22 +56,28 @@
             // 
             // dtgListview
             // 
+            this.dtgListview.AllowUserToAddRows = false;
+            this.dtgListview.AllowUserToDeleteRows = false;
             this.dtgListview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dtgListview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgListview.Location = new System.Drawing.Point(42, 81);
+            this.dtgListview.Location = new System.Drawing.Point(48, 108);
+            this.dtgListview.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dtgListview.MultiSelect = false;
             this.dtgListview.Name = "dtgListview";
+            this.dtgListview.ReadOnly = true;
+            this.dtgListview.RowHeadersWidth = 51;
             this.dtgListview.RowTemplate.Height = 25;
-            this.dtgListview.Size = new System.Drawing.Size(545, 242);
+            this.dtgListview.Size = new System.Drawing.Size(623, 323);
             this.dtgListview.TabIndex = 2;
             this.dtgListview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgListview_CellContentClick);
             this.dtgListview.DoubleClick += new System.EventHandler(this.dtgListview_DoubleClick);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(280, 331);
+            this.button2.Location = new System.Drawing.Point(320, 441);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(86, 31);
             this.button2.TabIndex = 3;
             this.button2.Text = "Thêm";
             this.button2.UseVisualStyleBackColor = true;
@@ -77,14 +85,15 @@
             // 
             // AddKhachHang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 366);
+            this.ClientSize = new System.Drawing.Size(722, 488);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dtgListview);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tb_timkiem);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "AddKhachHang";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddKhachHang";
